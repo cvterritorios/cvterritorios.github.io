729cbd2ee1959946fd6116d9a7f0f8c2be14eb7f
MateusLuisMk19 <95911492+MateusLuisMk19@users.noreply.github.com>
2024-01-23 00:58:03 +0000

    update

--- js/pages/index.js
+++ js/pages/index.js
@@ -7,9 +7,44 @@ document.addEventListener("DOMContentLoaded", async () => {
   _html.elemento(
     "form",
     ["class"],
-    ["d-flex container mb-4 w-100"],
+    ["d-flex mb-3"],
     "conteudo",
-    `<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">`
+    `<input class="form-control" type="search" placeholder="Search" aria-label="Search">`
   );
+
+
+  _html.elemento("div",["class"],["d-flex justify-content-between p-0 m-0 mb-3"],"conteudo",
+  `
+  <button type="button" class="btn btn-outline-secondary"
+      style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .4rem; --bs-btn-font-size: .95rem;">
+    Adicionar
+  </button>
+
+  <button type="button" class="btn btn-outline-secondary"
+      style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .4rem; --bs-btn-font-size: .95rem;">
+    Filtrar
+  </button>
+
+  <button type="button" class="btn btn-outline-secondary"
+      style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .4rem; --bs-btn-font-size: .95rem;">
+    Selecionar
+  </button>
+
+  <button type="button" class="btn btn-outline-dark"
+      style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .4rem; --bs-btn-font-size: .95rem;">
+    Vista
+  </button>
+ `)
+
   html_Comp.bandeja();
 });
+
+// document.addEventListener("keydown", (e) => {
+//   e.key == "Enter" ? addTRT() : "";
+// });
+
+  
+function addTRT() {
+  alert("Oi");
+  document.removeEventListener("keydown")
+}
